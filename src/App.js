b819c31afab1c11e6d88d6efd6af3a18cadb8e9f
Leximiller128 <leximiller128@gmail.com>
2022-11-29 14:50:26 -0700

    intial commit, nav bar and footer

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./componets/Footer";
+import Navbar from "./componets/Navbar";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
