ba24da50711452f5951f2c7eb284a5d55b100c66
Leximiller128 <leximiller128@gmail.com>
2022-12-05 11:23:12 -0700

    adjusted photo and layout of aboutme

--- src/componets/Aboutme.js
+++ src/componets/Aboutme.js
@@ -4,9 +4,9 @@ import { MDBRow, MDBCol, MDBContainer } from "mdb-react-ui-kit";
 export default function Aboutme() {
   return (
     <MDBContainer>
-      <MDBRow>
-        <MDBCol lg="4" md="12" className="mb-4" center="center">
-          <img src={require("./assets/IMG_1229.jpg")} className="img-fluid" />
+      <MDBRow lg="12" md="12" className="mb-1">
+        <MDBCol lg="3" md="12" className="mb-4">
+          <img src={require("./assets/IMG_3540.jpg")} className="img-fluid" />
         </MDBCol>
       </MDBRow>
       I am a DU Fullstack Coding Bootcamp student from Boulder, CO. My previous
