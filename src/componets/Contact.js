fd4af195de68687db71d5b9799b0c196c679a82a
Leximiller128 <leximiller128@gmail.com>
2023-01-26 23:59:35 -0700

    added contact form

--- src/componets/Contact.js
+++ src/componets/Contact.js
@@ -1,88 +1,113 @@
-import React, { useState } from "react";
+import { MDBContainer, MDBRow } from "mdb-react-ui-kit";
+import React, { useEffect, useState } from "react";
 import { validateEmail } from "../utils/helpers";
 import "./styles/contact.css";
 
 function Contact() {
-  const [nameInput, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [content, setContent] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [contactName, setContactName] = useState("");
+  const [contactMessage, setContactMessage] = useState("");
 
   const handleInputChange = (e) => {
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
 
-    if (inputType === "email") {
+    if (inputType === "contactName") {
+      setContactName(inputValue);
+    } else if (inputType === "email") {
       setEmail(inputValue);
-    } else if (inputType === "name") {
-      setName(inputValue);
+    } else if (inputType === "contactMessage") {
+      setContactMessage(inputValue);
+    }
+  };
+
+  const nameLeave = (e) => {
+    const { target } = e;
+    if (!target.value) {
+      setErrorMessage("Please add your name");
     } else {
-      setContent(inputValue);
-      console.log(content);
+      setErrorMessage("");
     }
   };
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-
-    if (!validateEmail(email)) {
-      setErrorMessage("Invalid email, please try again");
-      return;
+  useEffect(() => {
+    if (validateEmail(email) || email === "") {
+      setErrorMessage("");
+    } else {
+      setErrorMessage("Please add your email");
     }
-    if (!nameInput) {
-      setErrorMessage("Please enter a name");
-      return;
-    }
-
-    if (!content) {
-      setErrorMessage("Please enter a message");
-      return;
-    }
-
-    setContent("");
-    setEmail("");
-    setName("");
-  };
+  }, [email]);
 
   return (
-    <section id="contact">
-      <h3>Contact Me!</h3>
+    <main className="contact-section">
       <form>
-        <p>Name:</p>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          onChange={handleInputChange}
-          value={nameInput}
-        />
-        <p>Email:</p>
-        <input
-          id="email"
-          name="email"
-          type="text"
-          onChange={handleInputChange}
-          value={email}
-        />
-        <p>Message:</p>
-        <textarea
-          id="content"
-          name="content"
-          type="text"
-          onChange={handleInputChange}
-          value={content}
-        />
-        <button type="button" id="submit-button" onClick={handleFormSubmit}>
-          Submit
-        </button>
-      </form>
-      {errorMessage && (
-        <div>
-          <p>{errorMessage}</p>
+        <p>Name</p>
+        <div className="container-2" id="container1">
+          <div className="form-floating entry">
+            <input
+              name="contactName"
+              type="text"
+              value={contactName}
+              className="form-control"
+              id="floatingName"
+              onChange={handleInputChange}
+              onMouseLeave={nameLeave}
+            ></input>
+          </div>
+
+          <p>Email address</p>
+          <div className="form-floating mb-3 entry" id="container1">
+            <input
+              name="email"
+              type="email"
+              value={email}
+              className="form-control"
+              onChange={handleInputChange}
+              id="floatingInput"
+              onMouseLeave={nameLeave}
+            ></input>
+          </div>
+
+          <p>Message</p>
+          <div className="form-floating textarea">
+            <textarea
+              name="contactMessage"
+              value={contactMessage}
+              className="form-control long"
+              onChange={handleInputChange}
+              id="floatingTextarea2"
+              onMouseLeave={nameLeave}
+            ></textarea>
+          </div>
+
+          <MDBRow>
+            <p className="error-text">{errorMessage}</p>
+            <button
+              className="btn-main btn2 hover-shadow"
+              id="btn-support"
+              type="sumbit"
+            >
+              Submit
+            </button>
+          </MDBRow>
+          <MDBRow>
+            <div className="contact-info">
+              <p>Contact Me:</p>
+              <p>Lexi Miller</p>
+              <p>leximiller128@gmail.com</p>
+              <p>Boulder, CO</p>
+              <a
+                className="email"
+                href="leximiller128@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              ></a>
+            </div>
+          </MDBRow>
         </div>
-      )}
-    </section>
+      </form>
+    </main>
   );
 }
 
